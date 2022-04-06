d97b3151a4fc82592671c0cdfcd19fd119620fc4
toddt <toddteis@gmail.com>
2022-04-06 10:11:14 +1000

    Added createTiles() that calls tileFactory() nine times to create the game board and returns an array. Added board[] that calls createTiles().

--- main.js
+++ main.js
@@ -2,16 +2,20 @@ const tileFactory = (name, row, column, marker) => {
     return { name, row, column, marker }
 }
 
-const board = [];
-
-board.push(tileFactory("topLeft", 1, 1, ''))
-board.push(tileFactory("topCentre", 1, 2, ''));
-board.push(tileFactory("topRight", 1, 3, ''));
-board.push(tileFactory("midLeft", 2, 1, ''));
-board.push(tileFactory("midCentre", 2, 2, ''));
-board.push(tileFactory("midRight", 2, 3, ''));
-board.push(tileFactory("botLeft", 3, 1, ''));
-board.push(tileFactory("botCenter", 3, 2, ''));
-board.push(tileFactory("botRight", 3, 3, ''));
+const board = createTiles();
 
 console.log(board);
+
+function createTiles() {
+    const arr = []
+    arr.push(tileFactory("topLeft", 1, 1, ''))
+    arr.push(tileFactory("topCentre", 1, 2, ''));
+    arr.push(tileFactory("topRight", 1, 3, ''));
+    arr.push(tileFactory("midLeft", 2, 1, ''));
+    arr.push(tileFactory("midCentre", 2, 2, ''));
+    arr.push(tileFactory("midRight", 2, 3, ''));
+    arr.push(tileFactory("botLeft", 3, 1, ''));
+    arr.push(tileFactory("botCenter", 3, 2, ''));
+    arr.push(tileFactory("botRight", 3, 3, ''));
+    return arr;
+}
