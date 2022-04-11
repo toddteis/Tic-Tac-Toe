82bc4d44c53c1f08c0e9fee09ccf812114deaea2
toddt <toddteis@gmail.com>
2022-04-11 15:49:25 +1000

    Added eslint linter and ran. Corrected problems.

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-const tileFactory = (name, row, column, marker) => {
-    return { name, row, column, marker }
+const tileFactory = (name, row, column, marker) => ({
+  name, row, column, marker,
+});
+
+function createTiles() {
+  const arr = [];
+  arr.push(tileFactory('topLeft', 1, 1, ''));
+  arr.push(tileFactory('topCentre', 1, 2, ''));
+  arr.push(tileFactory('topRight', 1, 3, ''));
+  arr.push(tileFactory('midLeft', 2, 1, ''));
+  arr.push(tileFactory('midCentre', 2, 2, ''));
+  arr.push(tileFactory('midRight', 2, 3, ''));
+  arr.push(tileFactory('botLeft', 3, 1, ''));
+  arr.push(tileFactory('botCenter', 3, 2, ''));
+  arr.push(tileFactory('botRight', 3, 3, ''));
+  return arr;
 }
 
 const board = createTiles();
 
 console.log(board);
-
-function createTiles() {
-    const arr = []
-    arr.push(tileFactory("topLeft", 1, 1, ''))
-    arr.push(tileFactory("topCentre", 1, 2, ''));
-    arr.push(tileFactory("topRight", 1, 3, ''));
-    arr.push(tileFactory("midLeft", 2, 1, ''));
-    arr.push(tileFactory("midCentre", 2, 2, ''));
-    arr.push(tileFactory("midRight", 2, 3, ''));
-    arr.push(tileFactory("botLeft", 3, 1, ''));
-    arr.push(tileFactory("botCenter", 3, 2, ''));
-    arr.push(tileFactory("botRight", 3, 3, ''));
-    return arr;
-}
