e9711aee7928b745f97364b22d8109ee05cf0b1d
toddt <toddteis@gmail.com>
2022-04-11 17:32:21 +1000

    Added inputController() & eventlistener() for all board tiles that call inputController().

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const boardTileTopLeft = document.getElementById('top-left');
+const boardTileTopCenter = document.getElementById('top-center');
+const boardTileTopRight = document.getElementById('top-right');
+const boardTileMidLeft = document.getElementById('mid-left');
+const boardTileMidCenter = document.getElementById('mid-center');
+const boardTileMidRight = document.getElementById('mid-right');
+const boardTileBotLeft = document.getElementById('bot-left');
+const boardTileBotCenter = document.getElementById('bot-center');
+const boardTileBotRight = document.getElementById('bot-right');
+
 const playerFactory = (name, mark) => ({
   name, mark,
 });
@@ -24,4 +34,44 @@ const board = createTiles();
 const playerA = playerFactory('player A', 'X');
 const playerB = playerFactory('player B', 'O');
 
+function inputController(para) {
+  console.log(para);
+}
+
 console.log(board, playerA, playerB);
+
+boardTileTopLeft.addEventListener('click', () => {
+  inputController(boardTileTopLeft.id);
+});
+
+boardTileTopCenter.addEventListener('click', () => {
+  inputController(boardTileTopCenter.id);
+});
+
+boardTileTopRight.addEventListener('click', () => {
+  inputController(boardTileTopRight.id);
+});
+
+boardTileMidLeft.addEventListener('click', () => {
+  inputController(boardTileMidLeft.id);
+});
+
+boardTileMidCenter.addEventListener('click', () => {
+  inputController(boardTileMidCenter.id);
+});
+
+boardTileMidRight.addEventListener('click', () => {
+  inputController(boardTileMidRight.id);
+});
+
+boardTileBotLeft.addEventListener('click', () => {
+  inputController(boardTileBotLeft.id);
+});
+
+boardTileBotCenter.addEventListener('click', () => {
+  inputController(boardTileBotCenter.id);
+});
+
+boardTileBotRight.addEventListener('click', () => {
+  inputController(boardTileBotRight.id);
+});
